de9ceb41cfd806c4099349d0c8c78eb7660d68fb
Carlos D Rodriguez <labs@desiderio.me.uk>
2017-12-03 11:29:35 +0000

    Adds progress bar and refactor flavor's xml files in order to reuse main layout section with the use of the merge tag

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import uk.me.desiderio.javajokes.Joker;
@@ -15,16 +16,23 @@ import uk.me.desiderio.jokedisplay.JokeDisplayActivity;
 
 
 public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.EndpointTaskListener {
-    private Joker joker;
+    private ProgressBar progressBar;
+    private View buttonContainerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        joker = new Joker();
+        progressBar = findViewById(R.id.joke_progress_bar);
+        buttonContainerView = findViewById(R.id.joke_request_button_container);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showJokeButton();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
 
     public void tellJoke(View view) {
         new EndpointsAsyncTask(this).execute();
+        showProgressBar();
     }
 
 
@@ -59,5 +68,16 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
         intent.putExtra(JokeDisplayActivity.EXTRA_JOKE, joke);
         startActivity(intent);
 
+    }
+
+    private void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+        buttonContainerView.setVisibility(View.GONE);
+    }
+
+
+    private void showJokeButton() {
+        progressBar.setVisibility(View.GONE);
+        buttonContainerView.setVisibility(View.VISIBLE);
     }
 }
