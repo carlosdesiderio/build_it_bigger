516b6bc1b4d64b863e2363fb14d80741f7c5b46e
Carlos D Rodriguez <labs@desiderio.me.uk>
2017-12-03 10:28:42 +0000

    Add functional test for the EndpointsAsyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,59 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask.EndpointTaskListener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+
+/**
+ * Functional test for {@link EndpointsAsyncTask}
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest{
+
+    private static final String TAG = EndpointsAsyncTask.class.getSimpleName();
+
+    private CountDownLatch latch;
+    private EndpointsAsyncTask task;
+    private String response;
+
+    @Before
+    public void setUp() {
+        latch = new CountDownLatch(1);
+        task = new EndpointsAsyncTask(new EndpointTaskListener() {
+            @Override
+            public void onJokeApiResponse(String joke) {
+                response = joke;
+                latch.countDown();
+                Log.d(TAG, "greencat: inisde RESPONSE");
+
+            }
+        });
+    }
+
+    @Test
+    public void whenTaskIsCalled_thenANotNullStringIsReturn() {
+        // test that is null initially
+        assertNull(response);
+        task.execute();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // test that response has been set after calling task
+        assertNotNull(response);
+    }
+}
